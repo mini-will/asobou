71e137eb5eff8bd4174da0edcdaa6a936f14702f
mini-will <apple0minimino@gmail.com>
2020-05-17 20:44:41 +0900

    [Update] API経由で取得したデータから表示へ変更

--- app/Http/Controllers/Api/PlayIndexController.php
+++ app/Http/Controllers/Api/PlayIndexController.php
@@ -12,7 +12,7 @@ class PlayIndexController extends Controller
         $hello = 'Hello,World!';
         $hello_array = ['Hello', 'こんにちは', 'ニーハオ'];
 
-        $play_all = array([
+        $play_all = ([
             [
                 "id" =>  1,
                 "title" => "どうぶつのもり",
@@ -21,16 +21,53 @@ class PlayIndexController extends Controller
             ],
             [
                 "id" => 2,
-                "title" => "おやつ",
+                "title" => "おやつ サーターアンダギー",
                 "src" => "http://img.youtube.com/vi/izjA7bqmH3k/hqdefault.jpg",
                 "flex" => 6
             ],
             [
                 "id" => 3,
-                "title" => "おやつ3",
-                "src" => "http://img.youtube.com/vi/izjA7bqmH3k/hqdefault.jpg",
+                "title" => "おやつ パンケーキ",
+                "src" => "http://img.youtube.com/vi/yvkRI8E5UdY/hqdefault.jpg",
                 "flex" => 6
             ],
+            [
+                "id" => 4,
+                "title" => "おりがみ つる",
+                "src" => "http://img.youtube.com/vi/qtP3TmvGUZk/hqdefault.jpg",
+                "flex" => 6
+            ],
+            [
+                "id" => 5,
+                "title" => "おりがみ すいっち",
+                "src" => "http://img.youtube.com/vi/GjyJsPMUDTA/hqdefault.jpg",
+                "flex" => 6
+            ], [
+                "id" => 6,
+                "title" => "おりがみ しゅりけん",
+                "src" => "http://img.youtube.com/vi/0_Ex9uXFI_A/hqdefault.jpg",
+                "flex" => 6
+            ], [
+                "id" => 7,
+                "title" => "なわとび",
+                "src" => "http://img.youtube.com/vi/2NNFyE3AtVg/sddefault.jpg",
+                "flex" => 6
+            ], [
+                "id" => 8,
+                "title" => "からだをうごかす すとらいだー",
+                "src" => "http://img.youtube.com/vi/lfQpg0a06XE/sddefault.jpg",
+                "flex" => 6
+            ], [
+                "id" => 9,
+                "title" => "からだをうごかす さんりんしゃ",
+                "src" => "http://img.youtube.com/vi/TWE8cjkNNj0/sddefault.jpg",
+                "flex" => 6
+            ], [
+                "id" => 10,
+                "title" => "えをかく ぬりええええええ",
+                "src" => "http://img.youtube.com/vi/npad23BoXic/hqdefault.jpg",
+                "flex" => 6
+            ]
         ]);
 
         // return ['apple' => 'red', 'peach' => 'pink'];
--- resources/js/components/PlayIndex.vue
+++ resources/js/components/PlayIndex.vue
@@ -4,114 +4,89 @@
             <br />
             <h2>あそびをえらんでね</h2>
 
-            <v-row>
-                <v-col
-                    v-for="card in cards"
-                    :key="card.title"
-                    :cols="card.flex"
-                    class="d-flex justify-center"
-                >
-                    <v-card>
-                        <v-responsive :aspect-ratio="16 / 9">
-                            <v-img
-                                :src="card.src"
-                                class="white--text align-end"
-                                gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-                                height="200px"
-                            >
-                                <v-card-title
-                                    v-text="card.title"
-                                ></v-card-title>
-                            </v-img>
+            <div v-if="!loading">
+                <v-row>
+                    <v-col
+                        v-for="playcard in playcards"
+                        :key="playcard.id"
+                        :cols="playcard.flex"
+                        class="d-flex justify-center"
+                    >
+                        <v-card>
+                            <v-responsive :aspect-ratio="16 / 9">
+                                <v-img
+                                    :src="playcard.src"
+                                    class="white--text align-end"
+                                    gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+                                    height="200px"
+                                >
+                                    <v-card-title
+                                        v-text="playcard.title"
+                                    ></v-card-title>
+                                </v-img>
 
-                            <v-card-actions>
-                                <v-spacer></v-spacer>
+                                <v-card-text>
+                                    <div class="text--primary">
+                                        sample text .......
+                                    </div>
+                                </v-card-text>
 
-                                <v-btn icon>
-                                    <v-icon>mdi-heart</v-icon>
-                                </v-btn>
+                                <v-card-actions>
+                                    <v-spacer></v-spacer>
 
-                                <v-btn icon>
-                                    <v-icon>mdi-bookmark</v-icon>
-                                </v-btn>
+                                    <v-btn icon>
+                                        <v-icon>mdi-heart</v-icon>
+                                    </v-btn>
 
-                                <v-btn icon>
-                                    <v-icon>mdi-share-variant</v-icon>
-                                </v-btn>
-                            </v-card-actions>
-                        </v-responsive>
-                    </v-card>
-                </v-col>
-            </v-row>
+                                    <v-btn icon>
+                                        <v-icon>mdi-bookmark</v-icon>
+                                    </v-btn>
+
+                                    <v-btn icon>
+                                        <v-icon>mdi-share-variant</v-icon>
+                                    </v-btn>
+                                </v-card-actions>
+                            </v-responsive>
+                        </v-card>
+                    </v-col>
+                </v-row>
+            </div>
+
+            <div v-else>Loading...</div>
         </v-container>
     </v-app>
 </template>
 
 <script>
 export default {
-    data: () => ({
-        cards: [
-            {
-                id: 1,
-                title: "どうぶつのもり",
-                src: "http://img.youtube.com/vi/YO-wTijsPcs/hqdefault.jpg",
-                flex: 6
-            },
-            {
-                id: 2,
-                title: "おやつ",
-                src: "http://img.youtube.com/vi/izjA7bqmH3k/hqdefault.jpg",
-                flex: 6
-            },
-            {
-                id: 3,
-                title: "おやつ パンケーキ",
-                src: "http://img.youtube.com/vi/yvkRI8E5UdY/hqdefault.jpg",
-                flex: 6
-            },
-            {
-                id: 4,
-                title: "おりがみ つる",
-                src: "http://img.youtube.com/vi/qtP3TmvGUZk/hqdefault.jpg",
-                flex: 6
-            },
-            {
-                id: 5,
-                title: "おりがみ　すいっち",
-                src: "http://img.youtube.com/vi/GjyJsPMUDTA/hqdefault.jpg",
-                flex: 6
-            },
-            {
-                id: 6,
-                title: "おりがみ しゅりけん",
-                src: "http://img.youtube.com/vi/0_Ex9uXFI_A/hqdefault.jpg",
-                flex: 6
-            },
-            {
-                id: 7,
-                title: "なわとび",
-                src: "http://img.youtube.com/vi/2NNFyE3AtVg/sddefault.jpg",
-                flex: 6
-            },
-            {
-                id: 8,
-                title: "からだをうごかす すとらいだー",
-                src: "http://img.youtube.com/vi/lfQpg0a06XE/sddefault.jpg",
-                flex: 6
-            },
-            {
-                id: 9,
-                title: "からだをうごかす さんりんしゃ",
-                src: "http://img.youtube.com/vi/TWE8cjkNNj0/sddefault.jpg",
-                flex: 6
-            },
-            {
-                id: 10,
-                title: "えをかく みっきー",
-                src: "http://img.youtube.com/vi/npad23BoXic/hqdefault.jpg",
-                flex: 6
-            }
-        ]
-    })
+    data() {
+        return {
+            playcards: null,
+            loading: false
+            // cards: [
+            //     {
+            //         id: 1,
+            //         title: "どうぶつのもり",
+            //         src: "http://img.youtube.com/vi/YO-wTijsPcs/hqdefault.jpg",
+            //         flex: 6
+            //     },
+            //     {
+            //         id: 2,
+            //         title: "おやつ",
+            //         src: "http://img.youtube.com/vi/izjA7bqmH3k/hqdefault.jpg",
+            //         flex: 6
+            //     }
+            // ]
+        };
+    },
+    created() {
+        this.loading = true;
+        axios.get(`/api/playindex`).then(response => {
+            this.playcards = response.data;
+            this.loading = false;
+
+            console.log(this.playcards);
+        });
+    }
 };
 </script>
