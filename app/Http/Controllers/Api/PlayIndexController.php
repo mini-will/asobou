63bae429cedc7ba6dc8d43b498ea3f166e79c0a2
mini-will <apple0minimino@gmail.com>
2020-05-18 21:42:06 +0900

    [Update] laravel collectionからランダムに要素を取り出すへ変更

--- app/Http/Controllers/Api/PlayIndexController.php
+++ app/Http/Controllers/Api/PlayIndexController.php
@@ -9,70 +9,104 @@ class PlayIndexController extends Controller
 {
     public function index()
     {
-        $hello = 'Hello,World!';
-        $hello_array = ['Hello', 'こんにちは', 'ニーハオ'];
-
-        $play_all = ([
+        $play_all = collect([
             [
                 "id" =>  1,
                 "title" => "どうぶつのもり",
                 "src" => "http://img.youtube.com/vi/YO-wTijsPcs/hqdefault.jpg",
+                "category" => "game",
                 "flex" => 6
             ],
             [
                 "id" => 2,
                 "title" => "おやつ サーターアンダギー",
                 "src" => "http://img.youtube.com/vi/izjA7bqmH3k/hqdefault.jpg",
+                "category" => "oyatsu",
                 "flex" => 6
             ],
             [
                 "id" => 3,
                 "title" => "おやつ パンケーキ",
                 "src" => "http://img.youtube.com/vi/yvkRI8E5UdY/hqdefault.jpg",
+                "category" => "oyatsu",
                 "flex" => 6
             ],
             [
                 "id" => 4,
                 "title" => "おりがみ つる",
                 "src" => "http://img.youtube.com/vi/qtP3TmvGUZk/hqdefault.jpg",
+                "category" => "",
                 "flex" => 6
             ],
             [
                 "id" => 5,
                 "title" => "おりがみ すいっち",
                 "src" => "http://img.youtube.com/vi/GjyJsPMUDTA/hqdefault.jpg",
+                "category" => "",
                 "flex" => 6
             ], [
                 "id" => 6,
                 "title" => "おりがみ しゅりけん",
                 "src" => "http://img.youtube.com/vi/0_Ex9uXFI_A/hqdefault.jpg",
+                "category" => "",
                 "flex" => 6
             ], [
                 "id" => 7,
                 "title" => "なわとび",
                 "src" => "http://img.youtube.com/vi/2NNFyE3AtVg/sddefault.jpg",
+                "category" => "",
                 "flex" => 6
             ], [
                 "id" => 8,
                 "title" => "からだをうごかす すとらいだー",
                 "src" => "http://img.youtube.com/vi/lfQpg0a06XE/sddefault.jpg",
+                "category" => "",
                 "flex" => 6
             ], [
                 "id" => 9,
                 "title" => "からだをうごかす さんりんしゃ",
                 "src" => "http://img.youtube.com/vi/TWE8cjkNNj0/sddefault.jpg",
+                "category" => "",
                 "flex" => 6
             ], [
                 "id" => 10,
-                "title" => "えをかく ぬりええええええ",
+                "title" => "えをかく ぬりえ",
                 "src" => "http://img.youtube.com/vi/npad23BoXic/hqdefault.jpg",
+                "category" => "",
+                "flex" => 6
+            ],
+            [
+                "id" => 11,
+                "title" => "うんどうする らじおたいそう",
+                "src" => "http://img.youtube.com/vi/jPeHKtiFXIg/hqdefault.jpg",
+                "category" => "",
+                "flex" => 6
+            ],
+            [
+                "id" => 12,
+                "title" => "べんきょうする ひらがな",
+                "src" => "http://img.youtube.com/vi/GgKItOVLo8w/hqdefault.jpg",
+                "category" => "",
+                "flex" => 6
+            ],
+            [
+                "id" => 13,
+                "title" => "おやつ アイスクリーム",
+                "src" => "https://www.meiji.co.jp/products/icecream/assets/img/4902705125308_product.jpg",
+                "category" => "",
                 "flex" => 6
             ]
+
+
+
         ]);
 
+        //https://qiita.com/mikakane/items/76ae73990bf1ece9e7ae
+        //https://webty.jp/staffblog/production/post-2184/
+
         // return ['apple' => 'red', 'peach' => 'pink'];
-        // return [$hello_array];
-        // return [$play_all];
-        return response()->json($play_all);
+
+        // laravel collectionの機能を使ってcollectionからランダムに要素を取得
+        return response()->json($play_all->random(4));
     }
 }
--- resources/js/components/PlayIndex.vue
+++ resources/js/components/PlayIndex.vue
@@ -1,92 +1,89 @@
 <template>
-    <v-app>
-        <v-container>
-            <br />
-            <h2>あそびをえらんでね</h2>
+  <v-app>
+    <v-container>
+      <br />
+      <h2>あそびをえらんでね</h2>
 
-            <div v-if="!loading">
-                <v-row>
-                    <v-col
-                        v-for="playcard in playcards"
-                        :key="playcard.id"
-                        :cols="playcard.flex"
-                        class="d-flex justify-center"
-                    >
-                        <v-card>
-                            <v-responsive :aspect-ratio="16 / 9">
-                                <v-img
-                                    :src="playcard.src"
-                                    class="white--text align-end"
-                                    gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-                                    height="200px"
-                                >
-                                    <v-card-title
-                                        v-text="playcard.title"
-                                    ></v-card-title>
-                                </v-img>
+      <div v-if="!loading">
+        <v-row>
+          <v-col
+            v-for="playcard in playcards"
+            :key="playcard.id"
+            cols="12"
+            sm="6"
+            class="d-flex justify-center"
+          >
+            <v-card>
+              <v-responsive :aspect-ratio="16 / 9">
+                <v-img
+                  :src="playcard.src"
+                  class="white--text align-end"
+                  gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+                  height="200px"
+                >
+                  <v-card-title v-text="playcard.title" class="headline font-weight-bold"></v-card-title>
+                </v-img>
 
-                                <v-card-text>
-                                    <div class="text--primary">
-                                        sample text .......
-                                    </div>
-                                </v-card-text>
+                <v-card-text>
+                  <div class="text--primary">sample text .......</div>
+                </v-card-text>
 
-                                <v-card-actions>
-                                    <v-spacer></v-spacer>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
 
-                                    <v-btn icon>
-                                        <v-icon>mdi-heart</v-icon>
-                                    </v-btn>
+                  <v-btn icon>
+                    <v-icon>mdi-heart</v-icon>
+                  </v-btn>
 
-                                    <v-btn icon>
-                                        <v-icon>mdi-bookmark</v-icon>
-                                    </v-btn>
+                  <v-btn icon>
+                    <v-icon>mdi-bookmark</v-icon>
+                  </v-btn>
 
-                                    <v-btn icon>
-                                        <v-icon>mdi-share-variant</v-icon>
-                                    </v-btn>
-                                </v-card-actions>
-                            </v-responsive>
-                        </v-card>
-                    </v-col>
-                </v-row>
-            </div>
+                  <v-btn icon>
+                    <v-icon>mdi-share-variant</v-icon>
+                  </v-btn>
+                </v-card-actions>
+              </v-responsive>
+            </v-card>
+          </v-col>
+        </v-row>
+      </div>
 
-            <div v-else>Loading...</div>
-        </v-container>
-    </v-app>
+      <div v-else>Loading...</div>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            playcards: null,
-            loading: false
-            // cards: [
-            //     {
-            //         id: 1,
-            //         title: "どうぶつのもり",
-            //         src: "http://img.youtube.com/vi/YO-wTijsPcs/hqdefault.jpg",
-            //         flex: 6
-            //     },
-            //     {
-            //         id: 2,
-            //         title: "おやつ",
-            //         src: "http://img.youtube.com/vi/izjA7bqmH3k/hqdefault.jpg",
-            //         flex: 6
-            //     }
-            // ]
-        };
-    },
-    created() {
-        this.loading = true;
-        axios.get(`/api/playindex`).then(response => {
-            this.playcards = response.data;
-            this.loading = false;
+  data() {
+    return {
+      playcards: null,
+      loading: false
+      // cards: [
+      //     {
+      //         id: 1,
+      //         title: "どうぶつのもり",
+      //         src: "http://img.youtube.com/vi/YO-wTijsPcs/hqdefault.jpg",
+      //         flex: 6
+      //     },
+      //     {
+      //         id: 2,
+      //         title: "おやつ",
+      //         src: "http://img.youtube.com/vi/izjA7bqmH3k/hqdefault.jpg",
+      //         flex: 6
+      //     }
+      // ]
+    };
+  },
+  created() {
+    this.loading = true;
+    axios.get(`/api/playindex`).then(response => {
+      this.playcards = response.data;
+      this.loading = false;
 
-            console.log(this.playcards);
-        });
-    }
+      console.log(this.playcards);
+    });
+  }
 };
 </script>
