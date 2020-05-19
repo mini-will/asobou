2b63b1969e189f02d22d6f5d9ecbcad37416cb6b
mini-will <apple0minimino@gmail.com>
2020-05-19 21:00:55 +0900

    [Update] カテゴリ別にjsonデータを返せるように変更

--- app/Http/Controllers/Api/PlayIndexController.php
+++ app/Http/Controllers/Api/PlayIndexController.php
@@ -14,99 +14,129 @@ class PlayIndexController extends Controller
                 "id" =>  1,
                 "title" => "どうぶつのもり",
                 "src" => "http://img.youtube.com/vi/YO-wTijsPcs/hqdefault.jpg",
-                "category" => "game",
+                "play_inout" => "in",
+                "play_category" => "ゲーム",
                 "flex" => 6
             ],
             [
                 "id" => 2,
                 "title" => "おやつ サーターアンダギー",
                 "src" => "http://img.youtube.com/vi/izjA7bqmH3k/hqdefault.jpg",
-                "category" => "oyatsu",
+                "play_inout" => "in",
+                "play_category" => "おやつ",
                 "flex" => 6
             ],
             [
                 "id" => 3,
                 "title" => "おやつ パンケーキ",
                 "src" => "http://img.youtube.com/vi/yvkRI8E5UdY/hqdefault.jpg",
-                "category" => "oyatsu",
+                "play_inout" => "in",
+                "play_category" => "おやつ",
                 "flex" => 6
             ],
             [
                 "id" => 4,
                 "title" => "おりがみ つる",
                 "src" => "http://img.youtube.com/vi/qtP3TmvGUZk/hqdefault.jpg",
-                "category" => "",
+                "play_inout" => "in",
+                "play_category" => "つくる",
                 "flex" => 6
             ],
             [
                 "id" => 5,
                 "title" => "おりがみ すいっち",
                 "src" => "http://img.youtube.com/vi/GjyJsPMUDTA/hqdefault.jpg",
-                "category" => "",
+                "play_inout" => "in",
+                "play_category" => "つくる",
                 "flex" => 6
             ], [
                 "id" => 6,
                 "title" => "おりがみ しゅりけん",
                 "src" => "http://img.youtube.com/vi/0_Ex9uXFI_A/hqdefault.jpg",
-                "category" => "",
+                "play_inout" => "in",
+                "play_category" => "つくる",
                 "flex" => 6
             ], [
                 "id" => 7,
                 "title" => "なわとび",
                 "src" => "http://img.youtube.com/vi/2NNFyE3AtVg/sddefault.jpg",
-                "category" => "",
+                "play_inout" => "out",
+                "play_category" => "運動",
                 "flex" => 6
             ], [
                 "id" => 8,
                 "title" => "からだをうごかす すとらいだー",
                 "src" => "http://img.youtube.com/vi/lfQpg0a06XE/sddefault.jpg",
-                "category" => "",
+                "play_inout" => "out",
+                "play_category" => "運動",
                 "flex" => 6
             ], [
                 "id" => 9,
                 "title" => "からだをうごかす さんりんしゃ",
                 "src" => "http://img.youtube.com/vi/TWE8cjkNNj0/sddefault.jpg",
-                "category" => "",
+                "play_inout" => "out",
+                "play_category" => "運動",
                 "flex" => 6
             ], [
                 "id" => 10,
                 "title" => "えをかく ぬりえ",
                 "src" => "http://img.youtube.com/vi/npad23BoXic/hqdefault.jpg",
-                "category" => "",
+                "play_inout" => "in",
+                "play_category" => "つくる",
                 "flex" => 6
             ],
             [
                 "id" => 11,
                 "title" => "うんどうする らじおたいそう",
                 "src" => "http://img.youtube.com/vi/jPeHKtiFXIg/hqdefault.jpg",
-                "category" => "",
+                "play_inout" => "in",
+                "play_category" => "運動",
                 "flex" => 6
             ],
             [
                 "id" => 12,
                 "title" => "べんきょうする ひらがな",
                 "src" => "http://img.youtube.com/vi/GgKItOVLo8w/hqdefault.jpg",
-                "category" => "",
+                "play_inout" => "in",
+                "play_category" => "勉強",
                 "flex" => 6
             ],
             [
                 "id" => 13,
                 "title" => "おやつ アイスクリーム",
                 "src" => "https://www.meiji.co.jp/products/icecream/assets/img/4902705125308_product.jpg",
-                "category" => "",
+                "play_inout" => "in",
+                "play_category" => "おやつ",
+                "flex" => 6
+            ],
+            [
+                "id" => 14,
+                "title" => "おやつ バナナ",
+                "src" => "https://www.sumifru.co.jp/img/trivia/nutrient-comment/img01.jpg",
+                "play_inout" => "in",
+                "play_category" => "おやつ",
+                "flex" => 6
+            ],
+            [
+                "id" => 15,
+                "title" => "おやつ ポテトチップス",
+                "src" => "https://koikeya.co.jp/potatochips/top_images/011801261018552266.png",
+                "play_inout" => "in",
+                "play_category" => "おやつ",
                 "flex" => 6
             ]
 
 
 
-        ]);
 
-        //https://qiita.com/mikakane/items/76ae73990bf1ece9e7ae
-        //https://webty.jp/staffblog/production/post-2184/
+
+        ]);
 
         // return ['apple' => 'red', 'peach' => 'pink'];
 
         // laravel collectionの機能を使ってcollectionからランダムに要素を取得
-        return response()->json($play_all->random(4));
+        // https://qiita.com/mikakane/items/76ae73990bf1ece9e7ae
+        // https://webty.jp/staffblog/production/post-2184/
+        return response()->json($play_all->where('play_category', 'おやつ')->random(1));
     }
 }
