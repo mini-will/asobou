c7cd717876fe5ac7b7ad8c6b47551f0c4e2fe4db
mini-will <apple0minimino@gmail.com>
2020-06-13 20:53:17 +0900

    [update] playinfo画面の移植

---
+++ resources/js/Pages/PlayInfo.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <v-container>
+      <v-row>
+        <v-col>
+          <router-link :to="{ name: 'PlayIndex' }">あそび一覧へ戻る</router-link>
+
+          <!-- <h1 class="my-4">{{ temp[0].display_name }}</h1> -->
+
+          <!-- <p>{{ this.getPlayCardsById(Number($route.params.id)) }}</p> -->
+          <!-- <p>{{ temp[0].kanji_name }}</p> -->
+          <!-- <p>{{ $route.params.id }}</p> -->
+
+          <!-- <youtube :video-id="temp[0].videoId" />
+
+          <p class="headline mt-4">どうぐ</p>
+          <p>{{ temp[0].tool_tags }}</p>
+
+          <p class="headline py-1">あそびかた</p>
+          <p>{{ temp[0].description }}</p>-->
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  created: function() {},
+  mounted: function() {},
+  computed: {}
+};
+</script>
+
