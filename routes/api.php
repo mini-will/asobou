8580c497bf80afe977494fc4dc77f00ae13ebc20
mini-will <apple0minimino@gmail.com>
2020-06-14 16:36:59 +0900

    [update] リファクタリング 利用しなくなったコントローラーを削除

--- routes/api.php
+++ routes/api.php
@@ -19,8 +19,6 @@ use Illuminate\Support\Facades\Route;
 // });
 
 
-Route::get('playindex', 'Api\PlayIndexController@show');
-
 Route::get('playproduct', 'Api\PlayProductController@index');
 Route::get('playproduct/{id}', 'Api\PlayProductController@show');
 Route::get('search', 'Api\PlayProductSearchController@show');
