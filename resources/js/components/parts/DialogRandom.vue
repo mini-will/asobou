bccfeb92cbf136a394ea95bb9bddf8a2d46fd568
mini-will <apple0minimino@gmail.com>
2020-08-01 20:05:13 +0900

    [update] ランダムにデータ取得する処理を動くように改修

--- resources/js/components/parts/DialogRandom.vue
+++ resources/js/components/parts/DialogRandom.vue
@@ -17,7 +17,7 @@
                   class="white--text align-end"
                   gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
                   height="200px"
-                  @click="selectMainPlay(playcard.id)"
+                  @click="selectMainPlay(playcard.id, playcard.category)"
                 >
                   <v-card-title v-text="playcard.display_name" class="headline font-weight-bold"></v-card-title>
                 </v-img>
@@ -27,7 +27,7 @@
         </v-row>
         <v-row>
           <v-col cols="12" sm="12" class="d-flex justify-center">
-            <v-btn @click="getPlayCardItem(2, 'exercise', 3)" justify="center">ほかの</v-btn>
+            <v-btn @click="getPlayCardItem(2, playCategory, 3)" justify="center">ほかの</v-btn>
           </v-col>
         </v-row>
       </div>
@@ -56,10 +56,28 @@ export default {
     };
   },
   created() {},
+  beforeMount() {},
   mounted() {
     this.getPlayCardItem(2, this.playCategory, 3);
+    console.log('mounted');
   },
   beforeUpdate() {},
+  updated() {
+    console.log(
+      'updated:' + this.dialogOn,
+      this.playId,
+      this.playCategory,
+      this.dialogPlayCards
+    );
+
+    // Dialogがonでplaycardデータが空の場合は、データ取得が必要な状態のためplaydataを取得する
+    if (this.dialogOn === true && this.dialogPlayCards === null) {
+      this.getPlayCardItem(2, this.playCategory, 3);
+    }
+  },
+  beforeDestroy() {
+    console.log('before destroy');
+  },
   computed: {},
   methods: {
     getPlayCardItem: function (random, category, old) {
@@ -75,7 +93,7 @@ export default {
       if (old != '') {
         this.query += '&old=' + old;
       }
-      console.log(this.query);
+      console.log('getPlayCardItem: query:' + this.query);
 
       // eslint-disable-next-line no-undef
       axios.get(`/api/playproduct?` + this.query).then((response) => {
@@ -90,10 +108,17 @@ export default {
       });
     },
     // 遊びカードを選択してダイアログを閉じる処理
-    selectMainPlay: function (playId) {
-      this.$emit('dialog-change', playId);
+    selectMainPlay: function (playId, playCategory) {
+      this.$emit('dialog-change', playId, playCategory);
+      console.log(
+        'dialog-change: playID ' + playId + ', playCategory ' + playCategory
+      );
+      this.dialogPlayCards = null;
     },
+    // ダイアログを閉じる処理
     chagenDialogOnOff() {
+      this.dialogPlayCards = null;
+      console.log('chagenDialogOnOff:' + this.dialogPlayCards);
       this.$emit('dialog-change', '');
     },
   },
