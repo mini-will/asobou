518fa2465b3f80e4299d588bb5ea8740aab8e96a
mini-will <apple0minimino@gmail.com>
2020-08-13 06:41:38 +0900

    [update] 全部の遊びページがv-containerで囲われていなかったので修正

--- resources/js/components/pages/PlayAllItem.vue
+++ resources/js/components/pages/PlayAllItem.vue
@@ -1,31 +1,33 @@
 <template>
   <div>
-    <h1 class="display-1 font-weight-bold ma-4">ぜんぶのあそび</h1>
+    <v-container>
+      <h1 class="display-1 font-weight-bold ma-4">ぜんぶのあそび</h1>
 
-    <v-row>
-      <v-col
-        v-for="(playcard, index) in playCards"
-        :key="index + playcard.id"
-        cols="12"
-        sm="6"
-        md="4"
-        lg="3"
-        class="d-flex justify-center my-4"
-      >
-        <v-card>
-          <router-link :to="{ name: 'PlayInfo', params: { id: playcard.id } }">
-            <v-img
-              :src="playcard.image_url"
-              class="white--text align-end"
-              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-              height="200px"
-            >
-              <v-card-title v-text="playcard.display_name" class="headline font-weight-bold"></v-card-title>
-            </v-img>
-          </router-link>
-        </v-card>
-      </v-col>
-    </v-row>
+      <v-row>
+        <v-col
+          v-for="(playcard, index) in playCards"
+          :key="index + playcard.id"
+          cols="12"
+          sm="6"
+          md="4"
+          lg="3"
+          class="d-flex justify-center my-4"
+        >
+          <v-card>
+            <router-link :to="{ name: 'PlayInfo', params: { id: playcard.id } }">
+              <v-img
+                :src="playcard.image_url"
+                class="white--text align-end"
+                gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+                height="200px"
+              >
+                <v-card-title v-text="playcard.display_name" class="headline font-weight-bold"></v-card-title>
+              </v-img>
+            </router-link>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
