d3a74345b7006d39ac7722f18eb16bacdd2d2b6c
mini-will <apple0minimino@gmail.com>
2020-08-23 15:20:03 +0900

    [add] twitter linkの設定

--- resources/js/app.js
+++ resources/js/app.js
@@ -17,6 +17,36 @@ import VueLoading from 'vue-loading-template'
 Vue.use(VueLoading, /** options **/ )
 
 
+import VueSocialSharing from 'vue-social-sharing'
+Vue.use(VueSocialSharing);
+
+// Font Awesome
+import {
+    library
+} from '@fortawesome/fontawesome-svg-core'
+
+import {
+    FontAwesomeIcon
+} from '@fortawesome/vue-fontawesome'
+
+import {
+    fas
+} from '@fortawesome/free-solid-svg-icons'
+
+import {
+    fab
+} from '@fortawesome/free-brands-svg-icons'
+
+import {
+    far
+} from '@fortawesome/free-regular-svg-icons'
+
+library.add(fas, far, fab)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+
+
 window.Vue = require('vue');
 
 // eslint-disable-next-line no-undef
